a3272e58ff35741dad4dce5a608ba5721a3a6a58
Elton Bezerra <elto_bs@hotmail.com>
2022-02-07 19:57:47 +0100

    update

--- src/main/java/be/com/elton/Automovel.java
+++ src/main/java/be/com/elton/Automovel.java
@@ -1,37 +1,44 @@
 package be.com.elton;
 
+import javax.faces.convert.FacesConverter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.Default;
 
+import be.com.elton.validation.groups.ValidacaoMinima;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
-@Entity @Table(name="automovel")
+@Entity
+@Table(name = "automovel")
+@FacesConverter("automovel")
 public class Automovel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String marca;
-	private String modelo;
+	
+	
+	private Modelo modelo;
+    @Min(1900)
+    @Max(message = "O maximo do ano da fabricaçao é: ", value = 0)
 	private Integer anoFabricacao;
+    @Min(1900)
+    @Max(value = 1, message = "O maximo do ano do modelo é {0}")
 	private Integer anoModelo;
+    @NotNull(groups = {ValidacaoMinima.class, Default.class})
+	private Double preco;
+	private Double kilometragem;
 	private String observacoes;
 
 	public Automovel() {
 		super();
-	}
-
-	public Automovel( String marca, String modelo, Integer anoFabricacao, Integer anoModelo,
-			String observacoes) {
-		super();
-		this.marca = marca;
-		this.modelo = modelo;
-		this.anoFabricacao = anoFabricacao;
-		this.anoModelo = anoModelo;
-		this.observacoes = observacoes;
 	}
 
 	public Long getId() {
@@ -40,22 +47,6 @@ public class Automovel {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getMarca() {
-		return marca;
-	}
-
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
 	}
 
 	public Integer getAnoFabricacao() {
@@ -81,8 +72,5 @@ public class Automovel {
 	public void setObservacoes(String observacoes) {
 		this.observacoes = observacoes;
 	}
-	
-
-		
 
 }
--- src/main/java/be/com/elton/AutomovelBean.java
+++ src/main/java/be/com/elton/AutomovelBean.java
@@ -1,5 +1,80 @@
 package be.com.elton;
 
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.transaction.Transaction;
+
+@ManagedBean
+@RequestScoped
 public class AutomovelBean {
+
+	private Automovel automovel = new Automovel();
+
+	private List<Automovel> automoveis;
+	private Marca marca;
+
+	public List<Automovel> getAutomoveis() {
+		if (this.automoveis == null) {
+			EntityManager em = JPAutil.getEntityManager();
+
+			Query q = em.createQuery("select a from Automovel a", Automovel.class);
+			this.automoveis = q.getResultList();
+			em.close();
+		}
+		return automoveis;
+
+	}
+	
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+
+	public Automovel getAutomovel() {
+		return automovel;
+	}
+
+	public void setAutomovel(Automovel automovel) {
+		this.automovel = automovel;
+	}
+
+	public void salva(Automovel automovel) {
+
+		EntityManager em = JPAutil.getEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(automovel);
+		em.getTransaction().commit();
+
+		em.close();
+		
+		
+
+	}
+
+	public void remove(Automovel automovel) {
+
+		EntityManager em = JPAutil.getEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+
+		tx.begin();
+		automovel = em.merge(automovel);
+		em.remove(automovel);
+		tx.commit();
+		em.close();
+
+	}
 
 }
